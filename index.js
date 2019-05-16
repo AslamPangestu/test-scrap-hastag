5daf8b0c8cfb09e6fb91f3044d62498654ba3344
aslampangestu <aslampangestu@gmail.com>
2019-05-16 16:19:01 +0700

    update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const api = require("./api");
 const mysql = require("mysql");
-const config = require("./config.js");
+const config = require("./db.js");
 const connection = mysql.createConnection(config);
 
 api
@@ -9,7 +9,6 @@ api
   .then(res => {
     var counter = 0;
     var data = res.data.graphql.hashtag.edge_hashtag_to_media.edges;
-    console.log(data[0]);
     while (counter < data.length) {
       var insert = {
         username: data[counter].node.owner.id,
@@ -18,11 +17,16 @@ api
         comment: data[counter].node.edge_media_to_comment.count,
         date: data[counter].node.taken_at_timestamp
       };
-      let sql = `INSERT INTO hastagig(username,content,like_count,comment,date)
-           VALUES(${insert.username},${insert.content},${insert.like_count},${insert.comment},${insert.userndateame})`;
+      console.log(insert);
+      let sql = `INSERT INTO hastagig(username,content,like_count,comment,date) VALUES(${
+        insert.username
+      },"${insert.content}",${insert.like_count},${insert.comment},${
+        insert.date
+      })`;
       connection.query(sql);
-      connection.end();
+      counter++;
     }
+    connection.end();
   })
   .catch(err => {
     console.log("ERR", err);
